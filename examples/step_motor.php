519d8f40e88111e4872e51a3404b8f0755d2d79b
markus <m.moeller@wikando.de>
2016-11-05 18:36:59 +0100

    Stepper test

--- examples/step_motor.php
+++ examples/step_motor.php
@@ -4,8 +4,12 @@ require_once '../lib/Gpio.class.php';
 
 $stepMotor = new StepMotor28byj48(14, 15, 18, 23);
 
-$stepMotor->turnDegreesLeft(90, 5000);
-$stepMotor->turnRightLeft(180, 5000);
+$stepMotor->turnDegreesLeft(180, 500);
+$stepMotor->turnDegreesRight(90, 500);
 
 $stepMotor->turnDegreesLeft(360, 1000);
-$stepMotor->turnRightLeft(360, 50000);
+$stepMotor->turnDegreesRight(360, 500);
+
+$stepMotor->turnDegreesLeft(90, 10000);
+
+$stepMotor->stop();
